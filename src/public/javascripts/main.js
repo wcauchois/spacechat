f8d0310c386f96f95027403b5a8996511a7fd4d6
Bill Cauchois <wcauchois@gmail.com>
2013-02-03 17:05:03 -0500

    new layout

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -1,10 +1,2 @@
 $(document).ready(function() {
-  doResize();
-  $('#chat').html(templates.loremIpsum({}));
 });
-$(window).resize(doResize);
-function doResize() {
-  $('#chat').css('width', ($(window).width() - 240) + 'px');
-  $('#chat').css('height', ($(window).height() - 150) + 'px');
-  $('#chat').scrollTop($('#chat').height());
-}
