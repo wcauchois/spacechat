af9cc541f6e00a8b651cb49a462fe30b34912c35
Bill Cauchois <wcauchois@gmail.com>
2013-02-04 20:00:49 -0500

    okay this is a much better layout

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -1,2 +1,8 @@
+function resizer() {
+  var paneHeight = $('#pane').height();
+  $('#chat').height(paneHeight - 100);
+}
+$(window).resize(resizer);
 $(document).ready(function() {
+  resizer();
 });
